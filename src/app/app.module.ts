b1e1216b277160591b707cb92a4900bbdc77a881
Arul Clinton <arulclinton@gmail.com>
2017-10-08 16:44:01 +0530

    Added Bootstrap & other modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: '', component:DashboardComponent},
+  { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [AuthGuard] },
+  { path: '', component: DashboardComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
@@ -22,9 +25,12 @@ const appRoutes: Routes = [
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
-    BrowserModule
+    BrowserModule, 
+    HttpModule,
+    FormsModule, 
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent  {
+  title = "MyAngular | Register Online";
 
-  constructor() { }
+  rForm: FormGroup;
+  post: any;
+ 
 
-  ngOnInit() {
+
+  constructor(private fb: FormBuilder) {
+    this.rForm = fb.group({
+      'firstName': [null, Validators.compose([
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(15)
+      ])],
+      'lastName': [null, Validators.compose([
+        Validators.required, 
+        Validators.minLength(2), 
+        Validators.maxLength(15)
+      ])],
+      'emailId': [null, Validators.compose([
+        Validators.required,
+        Validators.minLength(5)
+      ])],
+      'userName': [null, Validators.compose([
+        Validators.required,
+        Validators.minLength(5)
+      ])],
+      'password': [null, Validators.compose([
+        Validators.required, 
+        Validators.minLength(5)
+      ])],
+      'confirmPassword': [null, Validators.compose([
+        Validators.required,
+        Validators.minLength(5)
+      ])]
+    })
   }
 
+
+  onRegister(){
+    alert();
+
+  }
+}
+
+
+export class registerUser{
+  private firstName:string;
+  private lastName:string;
+  private userName:string;
+  private email:string;
+  private password:string;
 }
